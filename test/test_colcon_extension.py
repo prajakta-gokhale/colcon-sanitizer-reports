0a339c2c444c1e186b02be5ad5e4818276064ce9
Prajakta Gokhale <prajaktg@amazon.com>
2019-04-17 10:49:40 -0700

    Fix colcon extension unit tests

--- test/test_colcon_extension.py
+++ test/test_colcon_extension.py
@@ -3,10 +3,12 @@
 
 import argparse
 
-from colcon_core.event.test import TestFailure
+from colcon_core.event.job import JobEnded
 from colcon_core.event_handler import add_event_handler_arguments
 from colcon_core.event_handler import apply_event_handler_arguments
 from colcon_core.event_handler import get_event_handler_extensions
+from colcon_sanitizer_reports.event_handlers.asan_report \
+    import ASanReportEventHandler
 from colcon_sanitizer_reports.event_handlers.tsan_report \
     import TSanReportEventHandler
 from mock import Mock
@@ -16,32 +18,35 @@ from .entry_point_context import EntryPointContext
 
 
 def test_get_shell_extensions():
-    with EntryPointContext(tsan_report=TSanReportEventHandler):
+    with EntryPointContext(tsan_report=TSanReportEventHandler, asan_report=ASanReportEventHandler):
         extensions = get_event_handler_extensions(context=None)
-    assert list(extensions.keys()) == ['tsan_report']
+    assert list(extensions.keys()) == ['asan_report', 'tsan_report']
 
 
 def test_add_event_handler_arguments():
     parser = argparse.ArgumentParser()
-    with EntryPointContext(tsan_report=TSanReportEventHandler):
+    with EntryPointContext(tsan_report=TSanReportEventHandler, asan_report=ASanReportEventHandler):
         add_event_handler_arguments(parser)
     text = parser.format_help()
-    assert 'tsan_report+' in text
-    assert '* tsan_report:' in text
-    assert 'Generate a report of all' in text
+    assert 'asan_report- tsan_report-' in text
+    assert 'asan_report:' in text
+    assert 'tsan_report:' in text
 
 
 def test_apply_event_handler_arguments():
-    with EntryPointContext(tsan_report=TSanReportEventHandler):
+    with EntryPointContext(tsan_report=TSanReportEventHandler, asan_report=ASanReportEventHandler):
         extensions = get_event_handler_extensions(context=None)
-    assert extensions['tsan_report'].enabled is True
+    assert extensions['asan_report'].enabled is False
+    assert extensions['tsan_report'].enabled is False
 
     extensions['tsan_report'].enabled = None
+    assert extensions['asan_report'].enabled is False
     assert extensions['tsan_report'].enabled is None
 
     args = Mock()
-    args.event_handlers = ['tsan_report-']
+    args.event_handlers = ['asan_report+', 'tsan_report-']
     apply_event_handler_arguments(extensions, args)
+    assert extensions['asan_report'].enabled is True
     assert extensions['tsan_report'].enabled is False
 
 
@@ -49,17 +54,18 @@ def test_parsing_method_called():
     extension = TSanReportEventHandler()
     with patch(
         'colcon_sanitizer_reports.event_handlers.tsan_report.'
-        'TSanReportEventHandler._start_tsan_output_parsing'
+        'TSanReportEventHandler.__call__'
     ) as tsan_parser:
-        event = TestFailure(['executable'])
+        event = JobEnded(['test_communication'], 0)
         extension((event, None))
         assert tsan_parser.call_count == 1
 
         tsan_parser.reset_mock()
-        event = TestFailure(['executable'])
+        event = JobEnded(['test_rclcpp'], 0)
         extension((event, None))
         assert tsan_parser.call_count == 1
 
+        # Extension methods will get called but not handled for unknown event types.
         tsan_parser.reset_mock()
         extension(('unknown', None))
-        assert tsan_parser.call_count == 0
+        assert tsan_parser.call_count == 1
